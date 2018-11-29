25766d45fbf47977772378221b2b2d3d01cee51f
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2018-11-29 17:43:13 +0300

    [PICK to compiler branch] Refactor different entities from contextual contracts into strong hierarchies

--- plugins/contracts/contracts-plugin/src/org/jetbrains/kotlin/contracts/contextual/resolution/EffectDeclarations.kt
+++ plugins/contracts/contracts-plugin/src/org/jetbrains/kotlin/contracts/contextual/resolution/EffectDeclarations.kt
@@ -5,46 +5,51 @@
 
 package org.jetbrains.kotlin.contracts.contextual.resolution
 
-import org.jetbrains.kotlin.contracts.contextual.model.CleanerDeclaration
-import org.jetbrains.kotlin.contracts.contextual.model.ProviderDeclaration
-import org.jetbrains.kotlin.contracts.contextual.model.VerifierDeclaration
+import org.jetbrains.kotlin.contracts.contextual.model.*
 import org.jetbrains.kotlin.contracts.description.ExtensionEffectDeclaration
 import org.jetbrains.kotlin.contracts.description.expressions.ContractDescriptionValue
 import org.jetbrains.kotlin.contracts.description.expressions.FunctionReference
 import org.jetbrains.kotlin.contracts.description.expressions.VariableReference
 
+interface ContextualEffectDeclaration<T : ContextEntity, R : ContextEntityDeclaration<T>, O : ContractDescriptionValue> :
+    ExtensionEffectDeclaration {
+    val factory: R
+    val references: List<ContractDescriptionValue>
+    val owner: O
+}
+
 data class ContextProviderEffectDeclaration(
-    val factory: ProviderDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: FunctionReference
-) : ExtensionEffectDeclaration
+    override val factory: ProviderDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: FunctionReference
+) : ContextualEffectDeclaration<ContextProvider, ProviderDeclaration, FunctionReference>
 
 data class LambdaContextProviderEffectDeclaration(
-    val factory: ProviderDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: VariableReference
-) : ExtensionEffectDeclaration
+    override val factory: ProviderDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: VariableReference
+) : ContextualEffectDeclaration<ContextProvider, ProviderDeclaration, VariableReference>
 
 data class ContextVerifierEffectDeclaration(
-    val factory: VerifierDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: FunctionReference
-) : ExtensionEffectDeclaration
+    override val factory: VerifierDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: FunctionReference
+) : ContextualEffectDeclaration<ContextVerifier, VerifierDeclaration, FunctionReference>
 
 data class LambdaContextVerifierEffectDeclaration(
-    val factory: VerifierDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: VariableReference
-) : ExtensionEffectDeclaration
+    override val factory: VerifierDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: VariableReference
+) : ContextualEffectDeclaration<ContextVerifier, VerifierDeclaration, VariableReference>
 
 data class ContextCleanerEffectDeclaration(
-    val factory: CleanerDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: FunctionReference
-) : ExtensionEffectDeclaration
+    override val factory: CleanerDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: FunctionReference
+) : ContextualEffectDeclaration<ContextCleaner, CleanerDeclaration, FunctionReference>
 
 data class LambdaContextCleanerEffectDeclaration(
-    val factory: CleanerDeclaration,
-    val references: List<ContractDescriptionValue>,
-    val owner: VariableReference
-) : ExtensionEffectDeclaration
+    override val factory: CleanerDeclaration,
+    override val references: List<ContractDescriptionValue>,
+    override val owner: VariableReference
+) : ContextualEffectDeclaration<ContextCleaner, CleanerDeclaration, VariableReference>
